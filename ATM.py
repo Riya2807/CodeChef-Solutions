b844a44511ec2301be72b31f9455c4a74395f38f
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2021-10-01 23:01:34 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,15 +1,3 @@
-'''
-a=int(input())
-b=int(input())
-if a%5==0 and a<b:
-    balance=b-(a+0.50)
-    print(balance)
-elif a>b:
-    print(b)
-elif a%5!=0:
-    print(b)
-'''
-
 try:
     x,y = map(float, input().split())
     if x <= y-0.5 and x % 5==0:
