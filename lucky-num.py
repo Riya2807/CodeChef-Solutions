3200bc065e2ce5db889817740bbb4234bbbf30c7
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2021-10-15 22:10:38 +0530

    Add files via upload

---
+++ lucky-num.py
@@ -0,0 +1,11 @@
+try:
+    t=int(input())
+    for i in range(t):
+        num=input()
+        if '7' in num:
+            print('YES')
+        else:
+            print('NO')
+except Exception as e:
+    pass
+        
