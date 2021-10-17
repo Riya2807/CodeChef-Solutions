7746e0cdd3042008ec90f19239c14dfc4a15a10c
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2021-10-17 23:50:04 +0530

    Update TESTSERIES.py

--- TESTSERIES.py
+++ TESTSERIES.py
@@ -2,13 +2,13 @@ try:
     t=int(input())
     for i in range(t):
         str1= input()
-        c2=str1.count('1')
-        c3=str1.count('2')
-        if c2==c3 :
+        c1=str1.count('1')
+        c2=str1.count('2')
+        if c1==c2 :
             print('DRAW')
-        elif c2>c3:
+        elif c1>c2:
             print('INDIA')
-        elif c3>c2:
+        elif c2>c1:
             print('ENGLAND')
 except Exception as e:
     pass
